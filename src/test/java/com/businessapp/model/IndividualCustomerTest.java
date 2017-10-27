8604c1c41e4d8eac22ca078055a4a97f760aadbb
kroko <root.1drone@gmail.com>
2017-10-27 15:37:07 +0200

    added IndividualCustomerTest

---
+++ src/test/java/com/businessapp/model/IndividualCustomerTest.java
@@ -0,0 +1,26 @@
+package com.businessapp.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IndividualCustomerTest {
+    private IndividualCustomer customer;
+    @Before
+    public void setUp() throws Exception {
+        customer = new IndividualCustomer();
+    }
+
+    @Test
+    public void setGetFirstName() throws Exception {
+        customer.setFirstName("Meier");
+        assertEquals(customer.getFirstName(), "Meier");
+
+        customer.setFirstName(null);
+        assertEquals(customer.getFirstName(), null);
+
+        customer.setFirstName("");
+        assertEquals(customer.getFirstName(), "");
+    }
+}
