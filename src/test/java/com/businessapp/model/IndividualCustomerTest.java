8cbfe175378503a347b6c995385f6221adcb01ed
1r00t <root.1drone@gmail.com>
2017-11-03 14:56:55 +0100

    neue Tests

--- src/test/java/com/businessapp/model/IndividualCustomerTest.java
+++ src/test/java/com/businessapp/model/IndividualCustomerTest.java
@@ -3,6 +3,11 @@ package com.businessapp.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import static org.junit.Assert.*;
 
 public class IndividualCustomerTest {
@@ -18,10 +23,46 @@ public class IndividualCustomerTest {
         assertEquals(customer.getFirstName(), "Meier");
 
         customer.setFirstName(null);
-        assertEquals(customer.getFirstName(), null);
+        assertNull(customer.getFirstName());
 
-        
         customer.setFirstName("");
         assertEquals(customer.getFirstName(), "");
+    }
+
+    @Test
+    public void setGetId() throws Exception {
+        customer.setId("abc");
+        assertEquals(customer.getId(), "abc");
+
+        customer.setId(null);
+        assertNull(customer.getId());
+
+        customer.setId("");
+        assertEquals(customer.getId(), "");
+    }
+
+    @Test
+    public void setGetName() throws Exception {
+        customer.setName("abc");
+        assertEquals(customer.getName(), "abc");
+
+        customer.setName(null);
+        assertNull(customer.getName());
+
+        customer.setName("");
+        assertEquals(customer.getName(), "");
+    }
+
+    @Test
+    public void setGetCreated() throws Exception {
+
+        DateFormat format = new SimpleDateFormat("dd.mm.yyyy", Locale.GERMAN);
+        Date date = format.parse("03.11.2017");
+
+        customer.setCreated(date);
+        assertEquals(customer.getCreated(), date);
+
+        customer.setCreated(null);
+        assertNull(customer.getCreated());
     }
 }
